eaf2f2179c320707d82e949e74e39d42c70864ac
jasminesaligan <110054198+jasminesaligan@users.noreply.github.com>
2022-09-05 09:42:10 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Student me = new Student();
+
+        me.surname ="Saligan";
+        me.firstName = "Jasmine";
+        me.middleInitial = "A";
+        me.dateOfBirth = 03/21/03;
+        me.studentNumber = 2022100068;
+        me.studentEmailAddress = "saliganja@students.national-u.edu.ph";
+        me.iAmAwesome = true;
+        me.sayMyEmailAddress();
+        me.sayMyStudentNumber();
+        me.amIAwesome();
+
+       
+    }
+
+}
